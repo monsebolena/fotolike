009d8deb986eeafe1ea389106c1588b5a447bcdb
monsebolena <monsebaldo7@gmail.com>
2025-01-16 18:31:55 +0100

    login

--- registro.php
+++ registro.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulario de Registro</title>
+    <!-- Vinculando la hoja de estilos CSS -->
+    <link rel="stylesheet" href="registro.css">
 </head>
 <body>
-    <form action="" method="post">
-        <label for="nombre">Nombre</label>
-        <input type="text" name="apellido" id="apellido">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email">
-        <label for="password">Contraseña</label>
-        <input type="password" name="password" id="password">
-        <input type="submit" value="Registrarse">
-    </form>
+    <div class="container">
+        <form action="" method="post" class="formulario">
+            <h2>Registrarse</h2>
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="nombre" required>
+
+            <label for="apellido">Apellido</label>
+            <input type="text" name="apellido" id="apellido" required>
+
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+
+            <label for="password">Contraseña</label>
+            <input type="password" name="password" id="password" required>
+
+            <input type="submit" value="Registrarse">
+        </form>
+    </div>
 </body>
 </html>
